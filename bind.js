18158919e556fbf11681e6f415f30c549b0e0c9b
yanghanfei <yanghanfei@codemao.cn>
2020-02-29 17:24:37 +0800

    feat(function): bind comments

--- bind.js
+++ bind.js
@@ -6,6 +6,9 @@ Function.prototype.myBind = function(context) {
   // 获取除第一个参数之后的参数
   const args = Array.from(arguments).slice(1);
   // 原函数，也就是 this
+  // 非 new 调用，this 为正常函数调用的 this，视调用环境而定
+  // new 调用，this 为 new 返回的对象，假设为 obj
+  // 所以可以在下文通过判断 obj 的原型是否为原函数的原型来判断是否为 new 调用
   const fnToBeBound = this;
   // 空函数
   const fNOP = function() {}
